252f086314ad042f3a8d31905c2211436ce1739d
G.Z <zerodreaming@sina.com>
2016-03-27 17:30:20 +0800

    Panda类

---
+++ lsf999/lsf999/PandaCtr.h
@@ -0,0 +1,13 @@
+//
+//  PandaCtr.h
+//  lsf999
+//
+//  Created by G.Z on 16/3/27.
+//  Copyright © 2016年 刘尚斐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PandaCtr : UIViewController
+
+@end
---
+++ lsf999/lsf999/PandaCtr.m
@@ -0,0 +1,37 @@
+//
+//  PandaCtr.m
+//  lsf999
+//
+//  Created by G.Z on 16/3/27.
+//  Copyright © 2016年 刘尚斐. All rights reserved.
+//
+
+#import "PandaCtr.h"
+
+@interface PandaCtr ()
+
+@end
+
+@implementation PandaCtr
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
