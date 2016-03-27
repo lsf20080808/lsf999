57dee5a7f49f220d5fab25defc121fb4dc5b7483
lsf20080808 <258118945@qq.com>
2016-03-27 17:33:46 +0800

    chuangjian22

---
+++ lsf999/lsf999/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  lsf999
+//
+//  Created by App on 16/3/27.
+//  Copyright © 2016年 刘尚斐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ lsf999/lsf999/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  lsf999
+//
+//  Created by App on 16/3/27.
+//  Copyright © 2016年 刘尚斐. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
